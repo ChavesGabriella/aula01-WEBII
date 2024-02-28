0607284ee1a09fce87cca3b30996f78d7623d2c6
ChavesGabriella <gabriellasoeirooo@gmail.com>
2024-02-28 11:57:43 -0300

    aula02 CRUD - EIXO

--- Docker/start-project/app/Http/Controllers/EixoController.php
+++ Docker/start-project/app/Http/Controllers/EixoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\EixoRepository;
+use App\Models\Eixo;
 use Illuminate\Http\Request;
 
 class EixoController extends Controller
@@ -35,15 +36,18 @@ class EixoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $obj = new Eixo();
+        $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
+        $this->repository->save($obj);
+        return "<h1>Store - OK!</h1>";
     }
-
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
+        $data = $this->repository->findById($id);
+        return $data;
     }
 
     /**
@@ -59,7 +63,13 @@ class EixoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $obj = $this->repository->findById($id);
+        if (isset($obj)) {
+            $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
+            $this->repository->save($obj);
+            return "<h1>Upate - OK!</h1>";
+        }
+        return "<h1>Upate - Not found Eixo!</h1>";
     }
 
     /**
